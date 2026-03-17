f3be45ce255fbf591b11781dca429f6fa3ba20a1
edamame0147 <e.developer2024@gmail.com>
2026-03-17 20:09:53 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,66 +1,81 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
 let players = {};
 
 io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
     socket.on('joinGame', (data) => {
         players[socket.id] = {
-            x: 600, y: 600, id: socket.id,
-            charType: data.charType, userName: data.userName,
-            hp: 100, kills: 0, isInBush: false, bushId: null
+            id: socket.id,
+            x: Math.random() * 1000 + 100,
+            y: Math.random() * 1000 + 100,
+            charType: data.charType,
+            userName: data.userName,
+            hp: 100,
+            kills: 0
         };
         io.emit('currentPlayers', players);
+        io.emit('updateRanking', players);
     });
 
-    socket.on('playerMovement', (data) => {
+    socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
-            Object.assign(players[socket.id], data);
+            players[socket.id].x = movementData.x;
+            players[socket.id].y = movementData.y;
+            players[socket.id].isInBush = movementData.isInBush;
+            players[socket.id].bushId = movementData.bushId;
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
 
-    socket.on('shoot', (data) => { socket.broadcast.emit('enemyShoot', data); });
-    socket.on('ult', (data) => { socket.broadcast.emit('enemyUlt', data); });
-    socket.on('sendPin', (data) => { io.emit('showPin', data); });
+    socket.on('shoot', (data) => {
+        socket.broadcast.emit('enemyShoot', data);
+    });
+
+    socket.on('ult', (data) => {
+        socket.broadcast.emit('enemyUlt', data);
+    });
 
     socket.on('updateHP', (data) => {
         if (players[data.id]) {
             players[data.id].hp = data.hp;
-            io.emit('hpUpdate', data);
-            if (data.hp <= 0 && data.attackerId && players[data.attackerId]) {
+            if (data.hp <= 0 && players[data.attackerId]) {
                 players[data.attackerId].kills++;
                 io.emit('updateRanking', players);
             }
+            io.emit('hpUpdate', data);
         }
     });
 
     socket.on('respawnRequest', () => {
         if (players[socket.id]) {
             players[socket.id].hp = 100;
-            players[socket.id].x = Math.floor(Math.random() * 800) + 200;
-            players[socket.id].y = Math.floor(Math.random() * 800) + 200;
+            players[socket.id].x = Math.random() * 1000 + 100;
+            players[socket.id].y = Math.random() * 1000 + 100;
             io.emit('playerRespawned', players[socket.id]);
         }
     });
 
-　　socket.on('disconnect', () => {
-    if (players[socket.id]) {
-        console.log(`User disconnected: ${players[socket.id].userName} (${socket.id})`);
-        // 1. プレイヤーリストから削除
-        delete players[socket.id];
-        // 2. 他の全員に「この人を消して」と通知
-        io.emit('playerDisconnected', socket.id);
-        // 3. 最新のランキングを全員に送る（これでランキングからも消える）
-        io.emit('updateRanking', players);
-    }
-});
+    socket.on('sendPin', (data) => {
+        io.emit('showPin', data);
+    });
+
+    socket.on('disconnect', () => {
+        if (players[socket.id]) {
+            console.log('User disconnected:', socket.id);
+            delete players[socket.id];
+            io.emit('playerDisconnected', socket.id);
+            io.emit('updateRanking', players); // ランキングから即座に消去
+        }
+    });
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+http.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
